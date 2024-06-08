88300ec423b6771dc21772ab1e859512ac60ea16
airtong <airton-carvalho@outlook.com>
2024-06-07 23:31:51 -0300

    [feature] material design

--- script/index.js
+++ script/index.js
@@ -1,16 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Selecionar o ícone de sanduíche e o menu lateral
     const menuIcon = document.getElementById('menuIcon');
     const sideMenu = document.getElementById('sideMenu');
 
-    // Verificar se os elementos foram encontrados antes de adicionar o evento de clique
     if (menuIcon && sideMenu) {
-        // Adicionar evento de clique ao ícone de sanduíche
         menuIcon.addEventListener('click', function () {
-            // Alternar a classe 'open' para abrir ou fechar o menu
             sideMenu.classList.toggle('open');
         });
     } else {
         console.error('Elementos não encontrados.');
     }
 });
+
+function temaLim() {
+    document.documentElement.style.setProperty('--md-sys-color-primary', '#CEF09D');
+    document.documentElement.style.setProperty('--md-sys-color-on-primary', '#000000');
+    document.documentElement.style.setProperty('--md-sys-color-primary-container', '#4f6a93');
+    document.documentElement.style.setProperty('--md-sys-color-on-primary-container', '#002020');
+}
+
+function temaDark() {
+    document.documentElement.style.setProperty('--md-sys-color-primary', '#38184C');
+    document.documentElement.style.setProperty('--md-sys-color-on-primary', '#FFFFFF');
+    document.documentElement.style.setProperty('--md-sys-color-primary-container', '#6FF7F6');
+    document.documentElement.style.setProperty('--md-sys-color-on-primary-container', '#002020');
+}
+
+function temaInatel() {
+    document.documentElement.style.setProperty('--md-sys-color-primary', '#126ae2');
+    document.documentElement.style.setProperty('--md-sys-color-on-primary', '#FFFFFF');
+    document.documentElement.style.setProperty('--md-sys-color-primary-container', '#6FF7F6');
+    document.documentElement.style.setProperty('--md-sys-color-on-primary-container', '#002020');
+}
--- script/login.js
+++ script/login.js
@@ -4,14 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
     loginForm.addEventListener('submit', function (event) {
         event.preventDefault();
 
-        // Aqui você pode adicionar a lógica de autenticação
         const username = loginForm.elements.username.value;
         const password = loginForm.elements.password.value;
 
-        // Exemplo simples de validação
         if (username === 'admin' && password === 'admin') {
             window.location.href = 'index.html';
-            // Aqui você pode redirecionar o usuário para outra página, se desejar
         } else {
             alert('Usuário ou senha incorretos.');
         }
