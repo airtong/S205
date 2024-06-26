9e14bfffa01f58373c31be48772a7fec96503ddc
Airton Carvalho <airtoncarvalho@wine.com.br>
2024-06-26 20:30:26 -0300

    update index

--- script/index.js
+++ script/index.js
@@ -31,3 +31,32 @@ function temaInatel() {
     document.documentElement.style.setProperty('--md-sys-color-primary-container', '#6FF7F6');
     document.documentElement.style.setProperty('--md-sys-color-on-primary-container', '#002020');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const carousel = document.querySelector('.carousel');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    carousel.addEventListener('mousedown', (e) => {
+        isDown = true;
+        startX = e.pageX - carousel.offsetLeft;
+        scrollLeft = carousel.scrollLeft;
+    });
+
+    carousel.addEventListener('mouseleave', () => {
+        isDown = false;
+    });
+
+    carousel.addEventListener('mouseup', () => {
+        isDown = false;
+    });
+
+    carousel.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - carousel.offsetLeft;
+        const walk = (x - startX) * 2; //scroll-fast
+        carousel.scrollLeft = scrollLeft - walk;
+    });
+});
